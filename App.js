90ba07f77f91aa82af85514ca8856a81d2894d1b
BryanVerneck <40044852+BryanVerneck@users.noreply.github.com>
2019-08-30 21:18:19 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -4,19 +4,45 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="Autenticacao">
-        <div>
-          <h1>Login</h1>
-        </div>
-        <div className="Login">
-          E-mail: <input type="text" placeholder="E-mail"/> <br/><br/>
-          Senha: <input type="text" placeholder="Senha"/><br/><br/>
-          <button>Login</button>
-        </div>
-      </div>
-      <div className="Autenticacao">
-        <h1>Cadastro</h1>
-      </div>
+        <form>
+          <div className="login">
+            <form>
+              <div>
+              <h1>Login</h1>
+              </div>
+              <div className="form-group">
+                E-mail: <input type="email" placeholder="E-mail"/>
+              </div>
+              <div className="form-group">
+                Senha: <input type="password" placeholder="Senha"/>
+              </div>
+              <button>Login</button><br/><br/>
+              <a href="#" onclick="mostrarCadastro()" className="cadastrar">Cadastrar-se</a>
+            </form>
+          </div>
+          <div className="cadastro">
+            <form>
+              <h1>Cadastro</h1>
+              <div className="form-group">
+                E-mail: <input type="email" placeholder="E-mail"/>
+              </div>
+              <div className="form-group">
+                Senha: <input type="password" placeholder="Senha"/><br/><br/>
+                Confirmar Senha: <input type="password" placeholder="Confirmar senha"/>
+              </div>
+              <div className="form-group">
+                CPF: <input type="text" placeholder="CPF"/>
+              </div>
+              <div className="form-group">
+                Sexo: <input type="radio" name="sexo"/> masculino <input type="radio" name="sexo"/> feminino
+              </div>
+              <div className="form-group">
+                Data de nascimento: <input type="date"/>
+              </div>
+              <button>Cadastrar</button>
+            </form>
+          </div>
+        </form>
     </div>
   );
 }
